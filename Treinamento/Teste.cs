51f332bba2ed278f35fb9c753522954c029e7d36
Wellissson Mendes Pereira dos Santos <wellisson.santos@eldorado.org.br>
2021-11-26 11:15:13 -0300

    Criação do projeto de UITesting

---
+++ Treinamento/Teste.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Treinamento
+{
+    public class Teste
+    {
+        [Test]
+        public void Test1()
+        {
+
+            string nomeString = "Wellisson";
+            var nomeVar = "Wellisson var";
+            object nomeObj = "Wellisson obj";
+
+
+            int numeroInt = 1;
+            var numeroVar = 2;
+            object numeroObj = 3;
+
+
+            bool eHomem = true;
+            var eMulher = false;
+            object eObj = true;
+
+            Console.WriteLine($"Olá! meu nome é {nomeString} e tenho {numeroVar} anos! ");
+        }
+
+    }
+}
